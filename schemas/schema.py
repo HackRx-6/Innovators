d68033ac9cf921cedfaf27d6e0868da7e94fac0e
Manish Jajoriya <manishjajoriya2005@gmail.com>
2025-08-23 07:44:50 +0530

    change schema to allow both type of url as input in body

--- schemas/schema.py
+++ schemas/schema.py
@@ -1,9 +1,14 @@
-import uuid
-from typing import List, Union
-
-from pydantic import BaseModel
-
+from typing import List, Optional
+from pydantic import BaseModel, model_validator
 
 class QueryIn(BaseModel):
-    documents : Union[List[str], str]
-    questions : List[str]
+    url: Optional[str] = None
+    document_url: Optional[str] = None
+    questions: List[str]
+
+    @model_validator(mode="after")
+    def check_one_present(cls, values):
+        url, document_url = values.url, values.document_url
+        if (url and document_url) or (not url and not document_url):
+            raise ValueError("Exactly one of 'url' or 'document_url' must be provided.")
+        return values
